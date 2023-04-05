8f30c22505b7bd2ca10f11c74d76f3d568007769
rnr1721 <admin@ehukr.com>
2023-04-05 18:58:22 +0300

    + able to overwrite response and template paths when get View from adapter

--- src/Interfaces/ViewAdapter.php
+++ src/Interfaces/ViewAdapter.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Core\Interfaces;
 
+use Psr\Http\Message\ResponseInterface;
+
 interface ViewAdapter
 {
 
     /**
      * Get View element ready for use
+     * @param array|string|null $templatePath Optional path to templates
+     * @param ResponseInterface|null Optional PSR ResponseInterface
      * @return View
      */
-    public function getView(): View;
+    public function getView(array|string|null $templatePath = null, ?ResponseInterface $response = null): View;
 }
